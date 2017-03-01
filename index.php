ef263932327e10b553f2bf6f7c611dcd9463ba0b
vjfernandez <victor.fernandezgil@educa.madrid.org>
2017-03-01 13:12:57 +0100

    Hecho index.php
    La aplicacion ya es funcional

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+</head>
+<body>
+Holaa
+</body>
+</html>
+<?php
+   include("vendor/autoload.php");
+   $log = new Monolog\Logger('name');
+   $log->pushHandler(new Monolog\Handler\StreamHandler('hello.log', Monolog\Logger::DEBUG));
+
+   $log->addInfo('Acceso a index.php');
+?>
